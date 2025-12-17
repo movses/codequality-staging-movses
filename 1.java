c8854fec5ca838df2c519684c8083d87f790d735
Movses Saponjyan <movses.saponjyan@gmail.com>
2025-12-17 19:01:42 +0100

    Update 1.java

--- 1.java
+++ 1.java
@@ -11,6 +11,7 @@ public class IvGenerator {
         return ivSpec;
     }
 
+    // This is entry point
     public static void main(String[] args) {
         IvParameterSpec ivSpec = generateIv();
         System.out.println("IV length: " + ivSpec.getIV().length);
