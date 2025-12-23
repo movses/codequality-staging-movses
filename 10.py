625d00ddd2cd9d5fde5d6a33e14aacde532cfdf4
Movses Saponjyan <movses.saponjyan@gmail.com>
2025-12-23 17:30:48 +0400

    Update 10.py

--- 10.py
+++ 10.py
@@ -35,3 +35,11 @@ if __name__ == "__main__":
     main()
 
 
+def g(a=[]):
+    for i in range(len(a)+1):
+        if a == None:
+            print(x)
+        y = 1 / 0
+        return a + 1
+
+g()
