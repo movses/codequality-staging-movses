b4995eeea160e2b90d357c53a280645c5c7a8f28
Movses Saponjyan <movses.saponjyan@gmail.com>
2025-12-23 17:58:34 +0400

    Update 12.py - division by zero issue

--- 12.py
+++ 12.py
@@ -25,3 +25,11 @@ if __name__ == "__main__":
 if __name__ == "__main__":
     main()
 
+
+
+def f(a=[]):
+    for i in range(len(a)+1):
+        if a == None:
+            print(x)
+        y = 1 / 0
+        return a + 1
