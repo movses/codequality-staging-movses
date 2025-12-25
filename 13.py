4c06ea8637ec97c148fc4464e3f8821ab8ce07b0
Movses Saponjyan <movses.saponjyan@gmail.com>
2025-12-25 15:39:48 +0400

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,4 +1,12 @@
 
+def f(a=[]):
+    for i in range(len(a)+1):
+        if a == None:
+            print(x)
+        y = 1 / 0
+        return a + 1
+
+
 def greet_user(name):
     print(f"Hello, {name}!")
     print("Welcome to our program.")
