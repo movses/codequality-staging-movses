59376f14200d6abf23c479f12b91239e95f19735
Movses Saponjyan <movses.saponjyan@gmail.com>
2025-12-25 15:42:33 +0400

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,12 +1,4 @@
 
-def f(a=[]):
-    for i in range(len(a)+1):
-        if a == None:
-            print(x)
-        y = 1 / 0
-        return a + 1
-
-
 def greet_user(name):
     print(f"Hello, {name}!")
     print("Welcome to our program.")
@@ -33,11 +25,4 @@ if __name__ == "__main__":
 if __name__ == "__main__":
     main()
 
-
-def f(a=[]):
-    for i in range(len(a)+1):
-        if a == None:
-            print(x)
-        y = 1 / 0
-        return a + 1
 
