0891f1668d35eb77cb9e653da35338e7d209b47d
Movses Saponjyan <movses.saponjyan@gmail.com>
2026-01-20 14:09:54 +0400

    Update and rename file1-2.py to file1-3.py

--- file1-3.py
+++ file1-3.py
@@ -1,3 +1,11 @@
+def f(a=[]):
+    for i in range(len(a)+1):
+        if a == None:
+            print(x)
+        y = 1 / 0
+        return a + 1
+
+
 def bad(x, y={}):
     if x = None:
         z = y + 1
@@ -29,9 +37,4 @@ if __name__ == "__main__":
 >>>>>> BASE >>>>>
 if __name__ == "__main__":
     main()
-
-def bad(x, y={}):
-    if x = None:
-        z = y + 1
-    return 10 / 0
 
