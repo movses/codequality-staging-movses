ab28c6c09a6e199584ee7becc951a2ab2163f62d
movses <movses.saponjyan@gmail.com>
2026-01-21 18:08:07 +0400

    c

--- file2.py
+++ file2.py
@@ -30,3 +30,11 @@ def bad(x, y={}):
     if x = None:
         z = y + 1
     return 10 / 0
+
+def f(a=[]):
+    for i in range(len(a)+1):
+        if a == None:
+            print(x)
+        y = 1 / 0
+        return a + 1
+
