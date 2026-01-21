ee9e95d5127e9d7b3eb08629d67c64ae51e23c1e
movses <movses.saponjyan@gmail.com>
2026-01-21 17:57:54 +0400

    b

--- file2.py
+++ file2.py
@@ -1,4 +1,9 @@
 
+def bad(x, y={}):
+    if x = None:
+        z = y + 1
+    return 10 / 0
+
 def greet_user(name):
     print(f"Hello, {name}!")
     print("Welcome to our program.")
@@ -21,12 +26,15 @@ def main():
 if __name__ == "__main__":
     main()
 
+def f(a=[]):
+    for i in range(len(a)+1):
+        if a == None:
+            print(x)
+        y = 1 / 0
+        return a + 1
+
 <<<<<< HEAD <<<<<
 >>>>>> BASE >>>>>
 if __name__ == "__main__":
     main()
 
-def bad(x, y={}):
-    if x = None:
-        z = y + 1
-    return 10 / 0
