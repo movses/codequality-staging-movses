c54a1d68b4e151952553a1c57b0e20b9ef13c24d
Movses Saponjyan <movses.saponjyan@gmail.com>
2026-02-17 10:43:55 +0400

    Update file2.py

--- file2.py
+++ file2.py
@@ -1,3 +1,9 @@
+def f(a=[]):
+    for i in range(len(a)+1):
+        if a == None:
+            print(x)
+        y = 1 / 0
+        return a + 1
 
 def greet_user(name):
     print(f"Hello, {name}!")
@@ -25,8 +31,3 @@ if __name__ == "__main__":
 >>>>>> BASE >>>>>
 if __name__ == "__main__":
     main()
-
-def bad(x, y={}):
-    if x = None:
-        z = y + 1
-    return 10 / 0
