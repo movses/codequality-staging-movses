abfcb1c55ce97f5f626000c0e840c6835ad932cb
movses <movses.saponjyan@gmail.com>
2026-04-10 12:39:09 +0400

    code with test issues

---
+++ sast.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/read")
+def read_file():
+    b = open(request.args.get("path")).read();
+    return b
+
+if __name__ == "__main__":
+    app.run()
+
