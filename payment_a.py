409cfdaf852c45e8bb99382daa34b7f5d220682e
movses <movses.saponjyan@gmail.com>
2026-04-29 19:27:14 +0400

    a

---
+++ payment_a.py
@@ -0,0 +1,11 @@
+  import logging
+
+  logger = logging.getLogger(__name__)
+
+
+  def charge_card(token, amount):
+      try:
+          response = call_payment_gateway(token, amount)
+          return response.transaction_id
+      except Exception:
+          pass
