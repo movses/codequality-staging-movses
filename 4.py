9cf7a9f18f8085795da88dd3d8fef039d0dc85ae
movses <movses.saponjyan@gmail.com>
2026-05-12 13:59:54 +0400

    18-3

--- 4.py
+++ 4.py
@@ -1,2 +1,19 @@
 print(x)
 print(y)
+
+def sloppy(a=[], b="1"):
+<<<<<<< HEAD
+    result = a + b
+=======
+    result = a - b
+>>>>>>> branch
+
+    if a == None:
+        pass
+        print("never runs")
+
+    temp = 123
+    temp = 456
+
+    return result
+    print("dead code")
