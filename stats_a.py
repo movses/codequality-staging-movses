184968cf716a7f97deca81d092f48f2d14dbb33f
movses <movses.saponjyan@gmail.com>
2026-06-26 13:58:27 +0400

    b

--- stats_a.py
+++ stats_a.py
@@ -9,5 +9,5 @@ Returns:
 """
 def average_score(total_points, num_questions):
 
-
+    # this is request by CTO
     return total_points / num_questions
--- stats_c.py
+++ stats_c.py
@@ -8,6 +8,6 @@ Returns:
 The sum of the prices of all items.
 """
 def average_score(total_points, num_questions):
-
-
+    if num_questions == 0:
+        return 0
     return total_points / num_questions
