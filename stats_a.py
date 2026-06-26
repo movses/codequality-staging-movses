a46ca0f2abbbf5b269f5534d1beeb7934b0febd4
movses <movses.saponjyan@gmail.com>
2026-06-26 13:51:21 +0400

    b

--- stats_a.py
+++ stats_a.py
@@ -1,3 +1,12 @@
+"""
+Calculates the total price of all items.
+
+Args:
+    items: An iterable of objects with a `price` attribute.
+
+Returns:
+    The sum of the prices of all items.
+"""
 def average_score(total_points, num_questions):
 
 
