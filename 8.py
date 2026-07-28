2f480b91314fe267ad37c1a890772e8153af71d4
movses <movses.saponjyan@gmail.com>
2026-07-28 15:09:55 +0400

    2

--- 8.py
+++ 8.py
@@ -9,3 +9,23 @@ def read_file():
 
 if __name__ == "__main__":
     app.run()
+
+
+
+
+def sloppy(a=[], b="1"):
+<<<<<<< HEAD
+    result = a + b
+=======
+    result = a - b
+>>>>>>> branch
+
+    if a == None:
+        pass
+        print("never runs")
+
+    temp = 123
+    temp = 456
+
+    return result
+    print("dead code")
