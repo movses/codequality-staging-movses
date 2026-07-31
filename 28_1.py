f5e9dcd3e04cacfa13ecde4fcc285332125ad312
movses <movses.saponjyan@gmail.com>
2026-07-31 15:15:42 +0400

    3

--- 28_1.py
+++ 28_1.py
@@ -1,15 +1,2 @@
-from flask import Flask, request
-import subprocess
-
-app = Flask(__name__)
-
-@app.route("/ping")
-def ping():
-    host = request.args.get("host")
-    return subprocess.check_output(f"ping -c 1 {host}", shell=True).decode()
-
-if __name__ == "__main__":
-    app.run()
-
 x = 1
 
--- 28_3.py
+++ 28_3.py
@@ -1,13 +1,2 @@
-from flask import Flask, request
-
-app = Flask(__name__)
-
-@app.route("/calc")
-def calc():
-    expr = request.args.get("expr")
-    return str(eval(expr))
-
-if __name__ == "__main__":
-    app.run()
 
 x = 1
