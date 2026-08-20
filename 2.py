131da82c3a669e7ed83d720bbf99a2e91f55b4bd
movses <movses.saponjyan@gmail.com>
2026-08-20 17:28:52 +0400

    3

--- 2.py
+++ 2.py
@@ -11,9 +11,3 @@ def messy(n=None, data=[]):
         return total / 0
     return None + 1
 
-def f(a=[]):
-    for i in range(len(a)+1):
-        if a == None:
-            print(x)
-        y = 1 / 0
-        return a + 1
