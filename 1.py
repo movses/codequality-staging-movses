e78e72a64f85120000151be86906d40826ba8f7e
movses <movses.saponjyan@gmail.com>
2026-09-07 13:01:25 +0400

    15

---
+++ 1.py
@@ -0,0 +1,41 @@
+# debug_me.py
+
+import os
+
+def greet(name:
+    print("Hello " + name
+
+if username == "admin"
+    print("Welcome!")
+
+for i in range(5):
+    print(i
+
+items = [1, 2, 3,]
+
+if (x > 10:
+    print("big")
+
+const user = {
+    name: "Bob",
+    age: 25
+};
+
+def calculate(x, y)
+    return x + y
+
+result = calculate(10, )
+
+while x < 10:
+    x++
+
+try:
+    do_something()
+except:
+    print("failed"
+    
+class Person
+    def __init__(self, name):
+        self.name = name
+
+print("finished"
